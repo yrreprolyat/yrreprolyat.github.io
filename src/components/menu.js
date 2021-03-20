103e801bca9de4f81bb6b356d618814aa349d8a2
tbp33 <tbp33@txstate.edu>
2021-03-20 14:14:32 -0500

    resume link

--- src/components/menu.js
+++ src/components/menu.js
@@ -260,7 +260,7 @@ const Menu = () => {
               </ol>
             )}
 
-            <a href="/resume.pdf" className="resume-link">
+            <a href="/Resume.pdf" className="resume-link">
               Resume
             </a>
           </nav>
