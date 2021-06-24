712b5a83d907dcb7bcd713490a3f32d7d2bb0d76
tbp33 <tbp33@txstate.edu>
2021-06-23 19:53:07 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-ebc530cde8ed2f0f8a72.js"
+    "url": "webpack-runtime-3866349595628c3642a7.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ae53827039e82f8ecddd19dbe1fefa7f"
+    "revision": "a3a19b1a130313decfa0c608c323130e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8033a24e61cb6e6f32a5.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "9105d83b34b9efe0cdc7fbd783051546"
+    "revision": "67027e2a2d4f380a2754fb6413ebcb54"
   },
   {
     "url": "polyfill-7e4d7804df080eb8f631.js"
