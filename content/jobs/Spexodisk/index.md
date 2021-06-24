4eada3e1cedbf97f3a6e00d3762b59b237571a11
tbp33 <tbp33@txstate.edu>
2021-06-23 19:59:16 -0500

    updates

---
+++ content/jobs/Spexodisk/index.md
@@ -0,0 +1,9 @@
+---
+title: 'Data Engineer'
+company: 'Faculty of Physics, Texas State University'
+location: 'Austin, TX'
+range: 'May 2021 - current'
+url: 'https://banzatti.wp.txstate.edu/spexodisks/'
+---
+
+
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -132,7 +132,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['C++','NodeJS', 'PostgreSQL', 'Python', 'Scikit-learn', 'Tensor flow'];
+  const skills = ['Python', 'MongoDB', 'Django', 'React', 'Tensor flow'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -141,7 +141,7 @@ const About = () => {
       <div className="inner">
         <StyledText>
           <div>
-            <p>I'm Taylor, an undergrad software engineer based in Austin, TX.</p>
+            <p>I'm Taylor, an Austin-based software engineer</p>
 
             <p>
             I am most passionate about building resilient data pipelines and robust applications
